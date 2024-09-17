1f0a6f8dd1e9b0bfe39091da71e95c1a7bf7223c
SAM V S <140595415+SAMVS2000@users.noreply.github.com>
2024-09-17 10:43:39 +0530

    Add files via upload

---
+++ StrLength.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>String Length in PHP</title>
+</head>
+<body>
+<?php
+$str1 = 'Hello world!';
+$len = strlen($str1);
+echo "Length of $str1 : $len";
+echo "<br>";
+?>
+</body>
+</html>
